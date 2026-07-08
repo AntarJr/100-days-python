01657ad5ccd35cfbaa21129ae2283d70ab714457
Ahmed Antar <antarjr414@gmail.com>
2026-07-08 21:24:27 +0300

    Complete Day 1

---
+++ Day 001/Day1.py
@@ -0,0 +1,4 @@
+print("Welcome to the band name generator.")
+city = input("Allow me to start by asking you.\nwhat's the name of the city you grew up in? \n")
+pet = input("what's the name of your pet? \n")
+print(f"your band name is {city} {pet}")
---
+++ Day 001/test1.py
@@ -0,0 +1,6 @@
+name = "ahmed"
+dream = "software engineer"
+path = "python"
+print(f"hello my name is {name}")
+print(f"i am studying {path}") 
+print(f"my dream is to be a {dream}")
